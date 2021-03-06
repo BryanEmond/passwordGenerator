c95ee913adb4a9dc94355f4c1aa4f2e3816c5854
BOR3 <bore22@outlook.com>
2021-03-06 16:10:28 -0500

    Add project files.

---
+++ passwordGenerator/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace passwordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cbNumeric = new System.Windows.Forms.CheckBox();
+            this.cbSpecialCharacters = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cbNumeric
+            // 
+            this.cbNumeric.AutoSize = true;
+            this.cbNumeric.Location = new System.Drawing.Point(348, 132);
+            this.cbNumeric.Name = "cbNumeric";
+            this.cbNumeric.Size = new System.Drawing.Size(65, 17);
+            this.cbNumeric.TabIndex = 3;
+            this.cbNumeric.Text = "Numeric";
+            this.cbNumeric.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cbNumeric.UseVisualStyleBackColor = true;
+            // 
+            // cbSpecialCharacters
+            // 
+            this.cbSpecialCharacters.AutoSize = true;
+            this.cbSpecialCharacters.Location = new System.Drawing.Point(348, 155);
+            this.cbSpecialCharacters.Name = "cbSpecialCharacters";
+            this.cbSpecialCharacters.Size = new System.Drawing.Size(114, 17);
+            this.cbSpecialCharacters.TabIndex = 4;
+            this.cbSpecialCharacters.Text = "Special characters";
+            this.cbSpecialCharacters.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cbSpecialCharacters.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(200, 68);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(400, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Number of letter in the password :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(471, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbSpecialCharacters);
+            this.Controls.Add(this.cbNumeric);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox cbNumeric;
+        private System.Windows.Forms.CheckBox cbSpecialCharacters;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ passwordGenerator/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passwordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox2.Text = trackBar1.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            string password = "";
+
+            Random rd = new Random();
+
+            int randNum;
+            int randLet;
+            int upOrLow;
+            int spenum;
+            int num;
+
+            char[] letter = new char[] { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+            char[] specialCar = new char[] { '~','!','@','#','$','%','^','*','-','_','=','+','[','{',']','}','/',';',':',',','.','?'};
+            char[] number = new char[] {'0','1','2','3','4','5','6','7','8','9'};
+
+            bool numeric = cbNumeric.Checked;
+            bool specialCarracters = cbSpecialCharacters.Checked;
+
+            while(password.Length != trackBar1.Value)
+            {
+                randNum = rd.Next(4);
+                if(randNum == 1)
+                {
+                    randLet = rd.Next(26);
+                    upOrLow = rd.Next(2);
+
+                    if(upOrLow == 0)
+                    {
+                        password += Char.ToUpper(letter[randLet]);
+                    }
+                    else
+                    {
+                        password += letter[randLet];
+                    }
+                }
+                if(randNum == 2 && numeric)
+                {
+                    num = rd.Next(10);
+                    password += number[num];
+                }
+                if(randNum == 3 && specialCarracters)
+                {
+                    spenum = rd.Next(22);
+                    password += specialCar[spenum];
+                }
+            }
+            textBox1.Text = password;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox2.Text = trackBar1.Value.ToString();
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter && Convert.ToInt32(textBox2.Text) < 101 && Convert.ToInt32(textBox2.Text) > 0)
+            {
+                trackBar1.Value = Convert.ToInt32(textBox2.Text);
+            }
+        }
+    }
+}
